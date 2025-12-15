cf81a4902e2e9e778e36250231cd977c6b9e4dd8
Abhinav Pratap Singh <abhinav.ism.iit@gmail.com>
2025-12-15 00:03:00 -0500

    tests : minor fix

--- test/phase_mixing_test.jl
+++ test/phase_mixing_test.jl
@@ -1,6 +1,6 @@
 @testitem "two_phase" tags = [:phase_mixing] begin
     using JET
-    m1 = two_phase_modelType(SEO3, Ni2011, HS1962_plus)
+    m1 = two_phase_modelType(SEO3, Gaillard2008, HS1962_plus)
     ps_nt = (; T=[1200.0f0, 1400.0f0] .+ 273, P=3.0f0, ρ=3300.0f0, Ch2o_m=100.0f0, ϕ=0.1f0)
     model = m1(ps_nt)
     @inferred m1(ps_nt)
